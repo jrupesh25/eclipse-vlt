bdf204bfdd9318fc2863fbdfc54cab28c103033d
unknown <Sergiy_Tymofiychuk@EPUAKHAW0023.kyiv.epam.com>
2011-12-23 14:50:25 +0200

    initial commit

---
+++ src/org/bitbucket/tsergey/vlt/handler/VltUpdateHandler.java
@@ -0,0 +1,21 @@
+package org.bitbucket.tsergey.vlt.handler;
+
+import java.util.Map;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class VltUpdateHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("======== Update ========");
+		
+		
+		return null;
+	}
+
+}

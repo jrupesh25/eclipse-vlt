d0ee59df0d868fcf734251f414e9f6ecfabb95c0
unknown <Sergiy_Tymofiychuk@EPUAKHAW0023.kyiv.epam.com>
2011-12-23 17:43:29 +0200

    add libraries

--- src/org/bitbucket/tsergey/vlt/handler/VltUpdateHandler.java
+++ src/org/bitbucket/tsergey/vlt/handler/VltUpdateHandler.java
@@ -2,9 +2,13 @@ package org.bitbucket.tsergey.vlt.handler;
 
 import java.util.Map;
 
+import org.apache.commons.cli2.CommandLine;
+import org.apache.commons.cli2.commandline.PropertiesCommandLine;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+
+import com.day.jcr.vault.cli.VaultFsApp;
 
 public class VltUpdateHandler extends AbstractHandler {
 
@@ -13,7 +17,11 @@ public class VltUpdateHandler extends AbstractHandler {
 		// TODO Auto-generated method stub
 		
 		System.out.println("======== Update ========");
+		VaultFsApp app = new VaultFsApp();
+		CommandLine cml = new PropertiesCommandLine(root, properties);
+		app.execute(cml);
 		
+//		VaultFsApp.main(new String[]{""});
 		
 		return null;
 	}

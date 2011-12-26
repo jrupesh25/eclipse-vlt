0cc1b45616a3e107788553a9f7d8f60a0660a4db
unknown <Sergiy_Tymofiychuk@EPUAKHAW0023.kyiv.epam.com>
2011-12-26 13:15:46 +0200

    remove libs, since it moved to vlt.libs project

--- src/org/bitbucket/tsergey/vlt/handler/VltCommitHandler.java
+++ src/org/bitbucket/tsergey/vlt/handler/VltCommitHandler.java
@@ -1,13 +1,12 @@
 package org.bitbucket.tsergey.vlt.handler;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-public class VltCommitHandler extends AbstractHandler {
+public class VltCommitHandler extends BaseHandler {
 
 	@Override
-	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+	public Object handle(ExecutionEvent arg0) throws ExecutionException {
 		// TODO Auto-generated method stub
 		
 		System.out.println("======== Commit ========");
--- src/org/bitbucket/tsergey/vlt/handler/VltUpdateHandler.java
+++ src/org/bitbucket/tsergey/vlt/handler/VltUpdateHandler.java
@@ -1,27 +1,17 @@
 package org.bitbucket.tsergey.vlt.handler;
 
-import java.util.Map;
-
-import org.apache.commons.cli2.CommandLine;
-import org.apache.commons.cli2.commandline.PropertiesCommandLine;
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
 import com.day.jcr.vault.cli.VaultFsApp;
 
-public class VltUpdateHandler extends AbstractHandler {
+public class VltUpdateHandler extends BaseHandler {
 
 	@Override
-	public Object execute(ExecutionEvent arg0) throws ExecutionException {
-		// TODO Auto-generated method stub
-		
+	public Object handle(ExecutionEvent arg0) throws ExecutionException {
 		System.out.println("======== Update ========");
-		VaultFsApp app = new VaultFsApp();
-		CommandLine cml = new PropertiesCommandLine(root, properties);
-		app.execute(cml);
-		
-//		VaultFsApp.main(new String[]{""});
+		VaultFsApp.main(new String[]{"up", "-v", "--force", "apps/fourseasons/components/booking/bk1-1-booking-flow-indicator/bk1-1-booking-flow-indicator.jsp"});
+		System.out.println("======== Success ========");
 		
 		return null;
 	}
